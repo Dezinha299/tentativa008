787908ad053f0ce8d22b9d5a65ed9ee8a65fc370
Dezinha299 <debora.rlarsen@gmail.com>
2026-04-20 19:55:59 -0300

    teste002
    tentativa002

--- Main.py
+++ Main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/teste1")
+async def funcaoteste():
+    return {"teste": "deu certo"}
+
+
